981d903362919554ca78806fdf195b60516e62a9
rahul.a.patel <rahul.a.patel@capgemini.com>
2025-11-27 12:16:01 +0530

    Rahul Patel Added controllers methods

--- CandidateSystem/src/main/java/com/example/CandidateSystem/controller/CandidateSystemContoller.java
+++ CandidateSystem/src/main/java/com/example/CandidateSystem/controller/CandidateSystemContoller.java
@@ -1,10 +1,72 @@
-package com.example.CandidateSystem.controller;
+package com.example.CandidateSystem.Controller;
 
+
+import com.example.CandidateSystem.Dto.Candidatedto;
+import com.example.CandidateSystem.Dto.PersonalDetailsdto;
+import com.example.CandidateSystem.Repo.PersonalDetailsRepo;
+import com.example.CandidateSystem.Service.CandidateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/Candidate")
 public class CandidateSystemContoller {
 
+    @Autowired
+    private CandidateService  candidateService;
 
-    public String function(){
-         return "Hi Rahul Patel";
+
+
+
+    @PostMapping("/add/candidate")
+    public ResponseEntity<Candidatedto> CreateCandidateDetails(@RequestBody Candidatedto candidatedto){
+        Candidatedto candidatedto1 = candidateService.createCandidate( candidatedto );
+        return ResponseEntity.status(HttpStatus.CREATED).body(candidatedto1);
     }
+
+    @PostMapping("/add/personal")
+    public ResponseEntity<PersonalDetailsdto> CreatePersonalDetails(@RequestBody PersonalDetailsdto personalDetailsdto){
+        PersonalDetailsdto personalDetailsdto1  = candidateService.createPersonalDetails(personalDetailsdto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(personalDetailsdto1);
+    }
+
+    @GetMapping("/get/personal")
+    public ResponseEntity<PersonalDetailsdto> GetPersonalDetails(@RequestParam Integer id){
+        PersonalDetailsdto personalDetailsdto = candidateService.getPersonalDetails(id);
+        return ResponseEntity.status(HttpStatus.OK).body(personalDetailsdto);
+    }
+
+    @GetMapping("/get/candidate")
+    public ResponseEntity<?> GetCandidateDetails(@RequestParam Integer id){
+       Candidatedto candidatedto = candidateService.getCandidate(id);
+       return ResponseEntity.status(HttpStatus.OK).body(candidatedto);
+    }
+
+    @DeleteMapping("/delete/candidate")
+    public ResponseEntity<?> DeleteCandidateDetails(@RequestParam Integer id){
+      candidateService.deleteCandidate(id);
+      return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping("/delete/personal")
+    public ResponseEntity<?> DeletePersonalDetails(@RequestParam Integer id){
+        candidateService.deletePersonalDetails(id);
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/update/candidate")
+    public ResponseEntity<Candidatedto> UpdateCandidateDetails(@RequestParam Integer id, @RequestBody Candidatedto candidatedto){
+        candidateService.updateCandidate(id, candidatedto);
+        return ResponseEntity.status(HttpStatus.OK).body(candidatedto);
+    }
+
+    @PutMapping("/update/personal")
+    public ResponseEntity<PersonalDetailsdto> UpdatePersonalDetails(@RequestParam Integer id ,@RequestBody PersonalDetailsdto personalDetailsdto){
+        candidateService.updatePersonalDetails(id, personalDetailsdto);
+        return ResponseEntity.status(HttpStatus.OK).body(personalDetailsdto);
+    }
+
 
 }
